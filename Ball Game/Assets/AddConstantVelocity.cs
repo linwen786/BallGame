d2b4e02db758ee920010366c86a6256eed5b351c
linwen786 <65876656+linwen786@users.noreply.github.com>
2020-07-26 20:46:40 -0700

    7/26/2020-8:46PM

---
+++ Ball Game/Assets/AddConstantVelocity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddConstantVelocity : MonoBehaviour
+
+    
+{
+    [SerializeField]
+    Vector3 v3Force;
+    // Start is called before the first frame update(Executed once at beggining)
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame(executed every fps)
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().velocity += new Vector3(0.1f, 0, 0);
+    }
+}
---
+++ Ball Game/Assets/AddPlayerControlledVelocity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPlayerControlledVelocity : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 v3Force;
+    [SerializeField]
+    KeyCode keyPositive;
+    [SerializeField]
+    KeyCode keyNegative;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.Getkey(keyPositive))
+            GetComponent<Rigidbody>().velocity += v3Force;
+        if (Input.Getkey(keyNegative))
+            GetComponent<Rigidbody>().velocity -= v3Force;
+    }
+}
